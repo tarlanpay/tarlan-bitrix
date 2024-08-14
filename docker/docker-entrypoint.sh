c98ff8b436d62f1e78cb3264725b9b966052c3a8
s.belik <s.belik@tarlanpayments.kz>
2024-08-14 21:17:09 +0500

    added button style changed docker-entrypoint.sh updated docker file

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,11 +2,22 @@
 
 set -e
 
-# if [ -d "/var/www/html/bitrix" ]; then
-#   #ln -s /tarlanpayments /var/www/html/wa-plugins/payment/tarlanpayments
-# fi
+dirr=/usr/local/bin/apache2-foreground
 
-/usr/local/bin/apache2-foreground
+for entry in "$dirr"/*
+do
+  echo "$entry"
+done
 
+export APACHE_RUN_DIR=/var/run/apache2$SUFFIX
+export APACHE_RUN_USER=www-data
+export APACHE_RUN_GROUP=www-data
+export APACHE_PID_FILE=/var/run/apache2/apache2.pid
+export APACHE_LOG_DIR=/var/log/apache2
+export APACHE_SERVER_NAME=localhost
+
+echo "ServerName localhost" >> /etc/apache2/apache2.conf
+
+apache2 -D FOREGROUND
 
 exec "$@"
--- tarlan.payments/install/sale_payment/tarlanpayments/template/template.php
+++ tarlan.payments/install/sale_payment/tarlanpayments/template/template.php
@@ -1,30 +1,32 @@
 <?
+
 use Bitrix\Main\Localization\Loc;
+
 \Bitrix\Main\Page\Asset::getInstance()->addCss("/bitrix/themes/.default/sale.css");
 Loc::loadMessages(__FILE__);
 
 $amount = round($params['PAYMENT_SHOULD_PAY'], 2);
 ?>
-<div class="mb-4" >
-    <p><?=Loc::getMessage('SALE_HANDLERS_PAY_SYSTEM_TARLANPAYMENTS_DESCRIPTION')." <strong>".SaleFormatCurrency($params['PAYMENT_SHOULD_PAY'], $payment->getField('CURRENCY'))."</strong>";?></p>
-    <form id="paysystem-tarlanpayments-form" name="PayForm" action="<?=$params['URL'];?>" method="post">
-        <input type="hidden" name="merchant_project_id" value="<?=$params['merchant_project_id'];?>">
-        <input type="hidden" name="callback_url" value="<?=$params['callback_url'];?>">
-        <input type="hidden" name="success_redirect_url" value="<?=$params['success_redirect_url'];?>">
-        <input type="hidden" name="failure_redirect_url" value="<?=$params['failure_redirect_url'];?>">
-        <input type="hidden" name="project_order_id" value="<?=$params['project_order_id'];?>">
-        <input type="hidden" name="project_reference_id" value="<?=$params['project_reference_id'];?>">
-        <input type="hidden" name="amount" value="<?=$amount;?>">
-        <input type="hidden" name="description" value="<?=$params['description'];?>">
-        <input type="hidden" name="project_hash" value="<?=$params['project_hash'];?>">
+<div id="tarlanpay-wrapper" class="mb-4">
+    <p><?= Loc::getMessage('SALE_HANDLERS_PAY_SYSTEM_TARLANPAYMENTS_DESCRIPTION') . " <strong>" . SaleFormatCurrency($params['PAYMENT_SHOULD_PAY'], $payment->getField('CURRENCY')) . "</strong>"; ?></p>
+    <form id="paysystem-tarlanpayments-form" name="PayForm" action="<?= $params['URL']; ?>" method="post">
+        <input type="hidden" name="merchant_project_id" value="<?= $params['merchant_project_id']; ?>">
+        <input type="hidden" name="callback_url" value="<?= $params['callback_url']; ?>">
+        <input type="hidden" name="success_redirect_url" value="<?= $params['success_redirect_url']; ?>">
+        <input type="hidden" name="failure_redirect_url" value="<?= $params['failure_redirect_url']; ?>">
+        <input type="hidden" name="project_order_id" value="<?= $params['project_order_id']; ?>">
+        <input type="hidden" name="project_reference_id" value="<?= $params['project_reference_id']; ?>">
+        <input type="hidden" name="amount" value="<?= $amount; ?>">
+        <input type="hidden" name="description" value="<?= $params['description']; ?>">
+        <input type="hidden" name="project_hash" value="<?= $params['project_hash']; ?>">
 
-        <input type="image" class="mw-100" name="submit">
+        <button class="btn btn-primary"><?= Loc::getMessage("SALE_HANDLERS_PAY_SYSTEM_TARLANPAYMENTS_BUTTON_PAID"); ?></button>
 
         <div class="d-flex align-items-center justify-content-start mb-4">
-            <p class="m-0 p-3"><?=Loc::getMessage('SALE_HANDLERS_PAY_SYSTEM_TARLANPAYMENTS_REDIRECT_MESS');?></p>
+            <p class="m-0 p-3"><?= Loc::getMessage('SALE_HANDLERS_PAY_SYSTEM_TARLANPAYMENTS_REDIRECT_MESS'); ?></p>
         </div>
 
-        <div class="alert alert-info"><?=Loc::getMessage('SALE_HANDLERS_PAY_SYSTEM_TARLANPAYMENTS_WARNING_RETURN');?></div>
+        <div class="alert alert-info"><?= Loc::getMessage('SALE_HANDLERS_PAY_SYSTEM_TARLANPAYMENTS_WARNING_RETURN'); ?></div>
     </form>
 </div>
 
